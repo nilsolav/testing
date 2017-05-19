ede3ba66a9477447e4507ee225b014817fa8ee61
nilsolav <nils.olav.handegard@imr.no>
2017-05-20 00:11:15 +0200

    Added support for the 70kHz files Added the pos reader and the files to work for the deployment 4.

--- REDUStokt/REDUStokt_WBATanalysis.m
+++ REDUStokt/REDUStokt_WBATanalysis.m
@@ -5,59 +5,44 @@ close all
 % Data directories
 d0 = 'D:\DATA\cruise_data';
 file{1} = fullfile(d0,'\2017\S2017836_PVendla[3670]\OTHER_PLATFORMS\WBAT\LSSS\Reports\ListUserFile04__F200000_T1_L0.0-0.0_HERR.txt');
-file{2} = fullfile(d0,'\2017\S2017836_PVendla[3670]\OTHER_PLATFORMS\WBAT70kHz\LSSS\Reports\ListUserFile04__F70000_T1_L0.0-0.0_HERR.txt');
+file{2} = fullfile(d0,'\2017\S2017836_PVendla[3670]\OTHER_PLATFORMS\WBAT70kHz\LSSS\Reports\ListUserFile04__F070000_T1_L0.0-0.0_HERR.txt');
 
-%% Import data
+% Import data
 % Import sa data
-[sa.dat,sa.sa,sa.time] = REDUStokt_importluf4(file{1});
-% Import pos files
-metadata_file='D:\DATA\cruise_data\2017\S2017836_PVendla[3670]\CRUISE_LOG\S2017836_metadata.xlsx';
+[sa{1}.dat,sa{1}.sa,sa{1}.time] = REDUStokt_importluf4(file{1});
+[sa{2}.dat,sa{2}.sa,sa{2}.time] = REDUStokt_importluf4(file{2});
 
+% Import metadata files
+metadata_file='D:\DATA\cruise_data\2017\S2017836_PVendla[3670]\CRUISE_LOG\S2017836_metadata.xlsx';
 [~,wbat,~] = REDUStokt_readmetadata(metadata_file);
 
-%% Depth table for WBAT deployment (taken from the echo sounder files)
+% Depth table for WBAT deployment (taken from the echo sounder files)
 depth_table =[datenum(2017,05,14,19,30,00) 58.5;...
     datenum(2017,05,14,23,15,00) 69.5;...
     datenum(2017,05,15,18,40,00) 69.5;...
     datenum(2017,05,16,19,30,00) 58.5];
 
 %% Get the WBAT and vessel profile per transect for the star experiment (and remove the Vendla passing times)
-s=size(sa.sa);
-for i=2:length(wbat)
-    for j=1:length(wbat(i).transect)
-        t0 = [wbat(i).transect(j).start.time wbat(i).transect(j).stop.time];
-        % Average time as time stamp
-        wbat(i).transect(j).avgtime = .5*(wbat(i).transect(j).stop.time+wbat(i).transect(j).start.time);
-        wbat(i).transect(j).avglat = .5*(wbat(i).transect(j).stop.lat+wbat(i).transect(j).start.lat);
-        wbat(i).transect(j).avglon = .5*(wbat(i).transect(j).stop.lon+wbat(i).transect(j).start.lon);
-        % wbat depth vector
-        
-        % Find depth of the transducer (channnel 1 = øverst i LSSS =
-        % nederst i vannsøyla)
-        %        depth_table(:,1)>t0(1)
-        dpth=depth_table(find(depth_table(:,1)<t0(1),1, 'last' ),2);
-        wbat(i).transect(j).wbat.depth = -(dpth - (1:s(1))*5);
-        %disp(wbat(i).transect(j).wbat.depth )
-        % Wbat sa values by depth
-        warning('Remove passings!')
-        tind = (t0(1)<sa.time.datenum)&(t0(2)>sa.time.datenum);
-        dum = sa.sa(:,tind);
-        wbat(i).transect(j).wbat.sabydepth = mean(dum,2);
-    end
-end
-%% Plot the WBAT star transects
+wbat = REDUStokt_depthdistributions(wbat,sa{1},depth_table,2:3);
+wbat = REDUStokt_depthdistributions(wbat,sa{2},depth_table,4);
 
+%% Plot the WBAT star transects
 close all
 figure
+col={'r','b','r','c'};
 
-col={'r','b','g'};
-
-for i=2:length(wbat)
+for i=2:4%length(wbat)
+    meansa=[];
     hold on
     for j=1:length(wbat(i).transect)
         plot(wbat(i).transect(j).wbat.sabydepth,wbat(i).transect(j).wbat.depth,col{i})
+        meansa = [meansa wbat(i).transect(j).wbat.sabydepth];
+        [~,nils]=max(wbat(i).transect(j).wbat.sabydepth);
+        text(wbat(i).transect(j).wbat.sabydepth(nils),wbat(i).transect(j).wbat.depth(nils),num2str(j))
     end
+    plot(mean(meansa,2),wbat(i).transect(j).wbat.depth,col{i},'LineWidth',2)
 end
+title('Raud=utsetting 3, Blå=utsetting 2 cyan=utsetting 4')
 
 %% Get the Vendla profile per Star transect
 
---
+++ REDUStokt/REDUStokt_depthdistributions.m
@@ -0,0 +1,26 @@
+function wbat = REDUStokt_depthdistributions(wbat,sa,depth_table,exprange)
+% This function creates depth profiles per transect
+
+s=size(sa.sa)
+for i=exprange
+    for j=1:length(wbat(i).transect)
+        t0 = [wbat(i).transect(j).start.time wbat(i).transect(j).stop.time];
+        % Average time as time stamp
+        wbat(i).transect(j).avgtime = .5*(wbat(i).transect(j).stop.time+wbat(i).transect(j).start.time);
+        wbat(i).transect(j).avglat = .5*(wbat(i).transect(j).stop.lat+wbat(i).transect(j).start.lat);
+        wbat(i).transect(j).avglon = .5*(wbat(i).transect(j).stop.lon+wbat(i).transect(j).start.lon);
+        % wbat depth vector
+        
+        % Find depth of the transducer (channnel 1 = øverst i LSSS =
+        % nederst i vannsøyla)
+        %        depth_table(:,1)>t0(1)
+        dpth=depth_table(find(depth_table(:,1)<t0(1),1, 'last' ),2);
+        wbat(i).transect(j).wbat.depth = -(dpth - (1:s(1))*5);
+        %disp(wbat(i).transect(j).wbat.depth )
+        % Wbat sa values by depth
+        %warning('Remove passings!')
+        tind = (t0(1)<sa.time.datenum)&(t0(2)>sa.time.datenum);
+        dum = sa.sa(:,tind);
+        wbat(i).transect(j).wbat.sabydepth = mean(dum,2);
+    end
+end
