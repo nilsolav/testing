5c80c4269d645e169f1a4e18be8c9403139f78ed
nilsolav <nils.olav.handegard@imr.no>
2017-05-19 15:21:06 +0200

    Working version plotting wbat vertical profile This version includes the code to compute sa by layer and time. The depth variable is also working.

--- REDUStokt/REDUStokt_WBATanalysis.m
+++ REDUStokt/REDUStokt_WBATanalysis.m
@@ -13,7 +13,7 @@ file{2} = fullfile(d0,'\2017\S2017836_PVendla[3670]\OTHER_PLATFORMS\WBAT70kHz\LS
 % Import pos files
 metadata_file='D:\DATA\cruise_data\2017\S2017836_PVendla[3670]\CRUISE_LOG\S2017836_metadata.xlsx';
 
-[~,wbat,time] = REDUStokt_readmetadata(metadata_file);
+[~,wbat,~] = REDUStokt_readmetadata(metadata_file);
 
 %% Depth table for WBAT deployment (taken from the echo sounder files)
 depth_table =[datenum(2017,05,14,19,30,00) 58.5;...
@@ -22,32 +22,53 @@ depth_table =[datenum(2017,05,14,19,30,00) 58.5;...
     datenum(2017,05,16,19,30,00) 58.5];
 
 %% Get the WBAT and vessel profile per transect for the star experiment (and remove the Vendla passing times)
-for i=1:length(wbat)
+s=size(sa.sa);
+for i=2:length(wbat)
     for j=1:length(wbat(i).transect)
-%        t0 = [wbat(i).transect(j).start wbat(i).transect(j).start];
+        t0 = [wbat(i).transect(j).start.time wbat(i).transect(j).stop.time];
         % Average time as time stamp
         wbat(i).transect(j).avgtime = .5*(wbat(i).transect(j).stop.time+wbat(i).transect(j).start.time);
         wbat(i).transect(j).avglat = .5*(wbat(i).transect(j).stop.lat+wbat(i).transect(j).start.lat);
         wbat(i).transect(j).avglon = .5*(wbat(i).transect(j).stop.lon+wbat(i).transect(j).start.lon);
         % wbat depth vector
-        wbat(i).transect(j).wbat.depth = 1;
+        
+        % Find depth of the transducer (channnel 1 = øverst i LSSS =
+        % nederst i vannsøyla)
+        %        depth_table(:,1)>t0(1)
+        dpth=depth_table(find(depth_table(:,1)<t0(1),1, 'last' ),2);
+        wbat(i).transect(j).wbat.depth = -(dpth - (1:s(1))*5);
+        %disp(wbat(i).transect(j).wbat.depth )
         % Wbat sa values by depth
-        wbat(i).transect(j).wbat.sabydepth = 1;
-        % Vessel depth vector
-        wbat(i).transect(j).vessel.depth = 1;
-        % Vessel sa values by depth
-        wbat(i).transect(j).vessel.sabydepth = 1;
+        warning('Remove passings!')
+        tind = (t0(1)<sa.time.datenum)&(t0(2)>sa.time.datenum);
+        dum = sa.sa(:,tind);
+        wbat(i).transect(j).wbat.sabydepth = mean(dum,2);
+    end
+end
+%% Plot the WBAT star transects
+
+close all
+figure
+
+col={'r','b','g'};
+
+for i=2:length(wbat)
+    hold on
+    for j=1:length(wbat(i).transect)
+        plot(wbat(i).transect(j).wbat.sabydepth,wbat(i).transect(j).wbat.depth,col{i})
     end
 end
 
 %% Get the Vendla profile per Star transect
+
 for i=1:length(wbat)
     for j=1:length(wbat(i).transect)
         t0 = [wbat(i).transect(j).start wbat(i).transect(j).start];
         % Vessel depth vector
         wbat(i).transect(j).vessel.depth = 1;
+        
         % Vessel sa values by depth
-        wbat(i).transect(j).vessel.sabydepth = 1;
+        wbat(i).transect(j).vessel.sabydepth = mean(dum,2);
     end
 end
 %% Get the wbat profile for Shale's circles
